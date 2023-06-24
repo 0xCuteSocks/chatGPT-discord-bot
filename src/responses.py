c1949987b2098d0ea77e8dbd63156d15a6790e65
0xCuteSocks <0xCuteSocks@tutanota.com>
2023-06-23 20:57:07 -0400

    Socks Fix.

--- src/responses.py
+++ src/responses.py
@@ -1,31 +1,21 @@
 import pprint as pp
 from EdgeGPT.EdgeGPT import ConversationStyle
 
-
 async def bing_handle_response(message, client) -> str:
     response = await client.bing_chatbot.ask(
         prompt=message, conversation_style=ConversationStyle.balanced
     )
     pp.pprint(response)
     try:
-        responseMessage = response["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]
-    except KeyError:
-        try:
-            responseMessage = response["item"]["messages"][-1]["adaptiveCards"][0]["body"][0]["text"]
-        except KeyError:
+        for i in range(1,5):
             try:
-                responseMessage = response["item"]["messages"][-1]["adaptiveCards"][0]["body"][1]["text"]
-            except KeyError:
-                responseMessage = (
-                    response["item"]["messages"][-1]["hiddenText"]
-                    + ", please use /reset_bing to reset."
-                )
-    except Exception as e:
-        print(e)
+                responseMessage = response["item"]["messages"][i]["adaptiveCards"][0]["body"][0]["text"]
+                return responseMessage
+            except:
+                continue
+    except:
         responseMessage = "Unknow error, please use /reset_bing to reset."
-
-    return responseMessage
-
+        return responseMessage
 
 async def bard_handle_response(message, client) -> str:
     response = await client.bard_chatbot.ask(message)
